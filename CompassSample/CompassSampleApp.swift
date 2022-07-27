79900398ad1a440d8136c6e4451e0487e9b1673b
Ryo <21cm0108@jec.ac.jp>
2022-07-27 18:11:53 +0900

    Initial Commit

---
+++ CompassSample/CompassSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompassSampleApp.swift
+//  CompassSample
+//
+//  Created by Ryo on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct CompassSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
